a88b55da09295a01bc204a56480bf2c685833ed7
elcoco <eelcokater@gmail.com>
2013-01-15 15:23:44 +0100

    databases

--- rss.py
+++ rss.py
@@ -6,6 +6,7 @@ import hashlib
 from log import Log
 from color import Color
 from time import strftime
+import sqlite3
 
 
 class GenerateHTML(object):
@@ -50,9 +51,58 @@ class GenerateHTML(object):
             # next would probably be the php file for marking everything as read
         
 
+class Database(object):
+    def create_tables(self):
+        db = sqlite3.connect(self.db_path)
+        db.execute("create table if not exists feeds (hash          text    unique  ,"
+                                                     "title         text            ,"
+                                                     "date_entered  text            ,"
+                                                     "feed_url      text            ,"
+                                                     "icon_url      text            ,"
+                                                     "last_updated  text            ,"
+                                                     "group_id      text            )")
 
-class RSS():
+        db.execute("create table if not exists entries (hash          text    unique  ,"
+                                                        "content      text            ,"
+                                                        "date_entered text            ,"
+                                                        "feed         text            )")
+
+        db.execute("create table if not exists groups (name        text            )")
+
+        db.commit()
+        db.close()
+
+
+    def get_table(self, table):
+    # Spits out the complete database into a list of dictionaries
+        db = sqlite3.connect(self.db_path)
+        rows = []
+        for row in db.execute('select ROWID,* from %s order by ROWID'%table):
+            rows.append(row)
+        db.close()
+        return rows
+
+
+    def insert_row(self, table, row):
+        # insert a group
+        db = sqlite3.connect(self.db_path)
+        values = []
+        for key in row:
+            values.append(row[key])
+        query = 'INSERT INTO %s VALUES(%s)'%(table, ','.join(['?'] * len(row)))
+        for k in row.keys():
+            print(query)
+            #db.execute("insert or ignore into %s(%s) values('%s')"%(table, k, str(row[k])))
+            db.execute(query,values)
+        db.commit()
+        self.log.debug('Row inserted')
+        db.close()
+
+
+
+class RSS(Database):
     def __init__(self):
+        self.db_path = '/home/eco/bin/apps/rss/rss.db'
         self.HTML_path = '/home/eco/bin/apps/rss/html'
         self.feedlist_path = '/home/eco/bin/apps/rss/feedslist'
         self.feeds_path = '/home/eco/bin/apps/rss/feeds'
@@ -60,6 +110,22 @@ class RSS():
         self.feeds = {}     # Contains feeds: key: hashed url, value: parsed feed object
         self.log = Log()
         self.color = Color()
+
+        # Map fields in tables to index in list so it's easier to change the order
+        self.group_id_field               = 0
+        self.group_title_field            = 1
+
+        self.entries_hash_field           = 0
+        self.entries_content_field        = 1
+        self.entries_date_entered_field   = 2
+        self.entries_feed_field           = 3
+
+        self.feeds_hash_field             = 0
+        self.feeds_title_field            = 1
+        self.feeds_date_entered_field     = 2
+        self.feeds_feed_url_field         = 3
+        self.feeds_last_updated_field     = 4
+        self.feeds_group_id_field         = 5
 
 
     def get_feedlist(self, path):
@@ -115,12 +181,6 @@ class RSS():
         except:
             self.log.error('Failed to parse feed: %s'%feed)
             return False
-
-
-    def get_dir_contents(self, path):
-        contents = []
-        for path,dirs,files in os.walk(path):
-            return files
 
 
     def parse_feedlist(self, feedlist):
@@ -179,23 +239,46 @@ class RSS():
         return data
 
 
-    def hash(self, data):
+    def get_hash(self, data):
         data = self.sanitize(data)
         data = self.encode(data)
         hashed = hashlib.sha224(data).hexdigest()
         return hashed
 
 
-    def run(self):
-        self.check_dir(self.feeds_path)
-        self.check_dir(self.template_path)
-        if not self.check_file(self.feedlist_path):
-            self.log.error('No feedlist found at: %s... Quitting'%self.feedlist_path)
+    def add_feed(self, url):
+        feed = self.parse_feed(url)
+        feed_insert = {}
+        if feed:
+            feed_insert['hash'] = str(self.get_hash(url))
+            feed_insert['title'] = str(feed.feed['title'])
+            feed_insert['date_entered'] = str(self.get_timestamp())
+            feed_insert['feed_url'] = str(url)
+            feed_insert['icon_url'] = str('None')
+            feed_insert['last_updated'] = str('None')
+            feed_insert['group_id'] = str(0)
 
-        feedlist = self.get_feedlist(self.feedlist_path)
-        self.feeds = self.parse_feedlist(feedlist)
-        html = GenerateHTML(self.template_path, self.feeds_path, self.HTML_path)
-        html.run()
+            print(feed_insert)
+            self.insert_row('feeds', feed_insert)
+
+
+            
+        
+
+
+    def run(self):
+        #self.check_dir(self.feeds_path)
+        #self.check_dir(self.template_path)
+        #if not self.check_file(self.feedlist_path):
+        #    self.log.error('No feedlist found at: %s... Quitting'%self.feedlist_path)
+
+        #feedlist = self.get_feedlist(self.feedlist_path)
+        #self.feeds = self.parse_feedlist(feedlist)
+        #html = GenerateHTML(self.template_path, self.feeds_path, self.HTML_path)
+        #html.run()
+        self.create_tables()
+        self.add_feed('http://iloveubuntu.net/rss.xml')
+        self.get_table('feeds')
 
 
 app = RSS()
