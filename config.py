82c79a6747d6814e113afe918eca587e66beb59a
elcoco <eelcokater@gmail.com>
2013-01-18 18:57:03 +0100

    cleaned code a little and some other stuff

--- config.py
+++ config.py
@@ -19,15 +19,52 @@ class Config(object):
         self.path_css                          = self.config_dir + '/css/stylesheet.css'
 
 
-        self.feeds                             = ['http://www.webupd8.org/feeds/posts/default', 
-                                                  'http://www.raspberrypi.org/feed', 
-                                                  'http://www.nasa.gov/rss/lg_image_of_the_day.rss', 
-                                                  'http://www.osnews.com/files/recent.xml', 
-                                                  'http://inconsolation.wordpress.com/feed/', 
-                                                  'http://iloveubuntu.net/rss.xml', 
-                                                  'https://bbs.archlinux.org/extern.php?action=feed&tid=92895&type=atom',
-                                                  'https://bbs.archlinux.org/extern.php?action=feed&tid=155476&type=atom',
-                                                  'http://feeds.bbci.co.uk/news/rss.xml']
+        self.feeds                             = [['http://www.webupd8.org/feeds/posts/default',                'linux'], 
+                                                  ['http://www.osnews.com/files/recent.xml',                    'linux'], 
+                                                  ['http://inconsolation.wordpress.com/feed/',                  'linux'], 
+                                                  ['http://librenix.com/rss/',                                  'linux'], 
+                                                  ['http://feeds.feedburner.com/d0od',                          'linux'], 
+                                                  ['http://feeds.feedburner.com/ostatic',                       'linux'], 
+                                                  ['http://www.tuxmachines.org/node/feed',                      'linux'], 
+                                                  ['http://tweakers.net/feeds/mixed.xml',                       'linux'], 
+                                                  ['http://feeds.webwereld.nl/webwereld',                       'linux'], 
+                                                  ['http://www.phoronix.com/rss.php',                           'linux'], 
+                                                  ['http://feeds.feedburner.com/Linuxers?format=xml',           'linux'], 
+                                                  ['http://lxer.com/module/newswire/headlines.rss',             'linux'], 
+                                                  ['http://rss.feedsportal.com/c/32569/f/491734/index.rss',     'linux'], 
+                                                  ['http://feeds.feedburner.com/thepowerbase?format=xml',       'linux'], 
+                                                  ['http://feeds.feedburner.com/unixmenhowtos?format=xml',      'linux'], 
 
-        self.groups                            = ['linux', 
-                                                  'news']
+                                                  ['http://www.nasa.gov/rss/lg_image_of_the_day.rss',                               'space'], 
+                                                  ['http://www.nasa.gov/rss/chandra_images.rss',                                    'space'], 
+                                                  ['http://www.esa.int/rss/TopNews.xml',                                            'space'], 
+                                                  ['http://www.nasa.gov/directorates/somd/reports/iss_reports/iss_reports.rss',     'space'], 
+                                                  ['http://www.nasa.gov/rss/breaking_news.rss',                                     'space'], 
+                                                  ['http://www.nasa.gov/mission_pages/SOFIA/sofia-newsandfeatures-RSS.rss',         'space'], 
+                                                  ['http://www.nasa.gov/rss/solar_system.rss',                                      'space'], 
+                                                  ['http://www.nasa.gov/rss/universe.rss',                                          'space'], 
+                                                  ['http://www.nasa.gov/mission_pages/kepler/news/kepler-newsandfeatures-RSS.rss',  'space'], 
+
+                                                  ['https://bbs.archlinux.org/extern.php?action=feed&tid=92895&type=atom',    'threads'],
+                                                  ['https://bbs.archlinux.org/extern.php?action=feed&tid=155476&type=atom',   'threads'],
+
+                                                  ['http://feeds.bbci.co.uk/news/rss.xml',                          'news'],
+                                                  ['http://www.haarlemsdagblad.nl/?service=rss',                    'news'],
+                                                  ['http://www.haarlemsdagblad.nl/nieuws/regionaal/?service=rss',   'news'],
+                                                  ['http://feeds.nos.nl/nosnieuwsalgemeen',                         'news'],
+                                                  ['http://nrc.nl/rss.php',                                         'news'],
+                                                  ['http://www.nu.nl/feeds/rss/algemeen.rss',                       'news'],
+                                                  ['http://lifehacker.com/index.xml',                               'news'],
+
+                                                  ['http://elementaryos.org/journal/rss.xml',                           'projects'],
+                                                  ['http://www.raspberrypi.org/feed',                                   'projects'],
+                                                  ['http://themagpi.wordpress.com/feed/',                               'projects'],
+                                                  ['https://www.haiku-os.org/rss.xml',                                  'projects'],
+                                                  ['https://www.tizen.org/blogs/feed',                                  'projects'],
+                                                  ['http://www.cyanogenmod.com/feed',                                   'projects'],
+                                                  ['http://fritzing.org/news/feeds/rss/',                               'projects'],
+
+                                                  ['http://quicksurf.com/?feed=ogg&amp;cat=141',                        'podcasts'],
+                                                  ['http://feeds.feedburner.com/linuxoutlaws',                          'podcasts']]
+
+        self.groups                            = ['linux', 'projects', 'news', 'space', 'threads', 'reddit']
