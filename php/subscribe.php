62726aca802a9db2de4aacce5df4fc2e3a555485
elcoco <eelcokater@gmail.com>
2013-03-26 18:34:24 +0000

    squashed bug when subscribing to certain feeds

--- php/subscribe.php
+++ php/subscribe.php
@@ -2,7 +2,7 @@
     $url = $_GET["url"];
     $group = $_GET["group"];
 
-    $handle = popen("LANG=en_US.UTF-8 $app_dir/static-rss --subscribe=$url --group=$group", 'r');
+    $handle = popen("LANG=en_US.UTF-8 $app_dir/static-rss --subscribe='$url' --group=$group", 'r');
     pclose ($handle);
     header ("location: $url");
 ?>
